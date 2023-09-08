62afa99ffb68f44c17e84ec980eb4c89ea80e53c
randyvergil <randy.davila@testpro.io>
2023-09-07 22:10:53 -0400

    Attempt #1

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,45 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest{
+
+    @Test
+
+    public void addSongToPlaylist() throws InterruptedException {
+
+    String newSongAddedNotificationText = "Added one song int";
+
+    navigateToPage();
+    provideEmail("randy.davila@testpro.io");
+    providePassword("te$t$tudent");
+    clickSubmit();
+    searchSong(songTitleKeyword: "Lament");
+    clickViewAllBtn();
+    selectFirstSongResult();
+    clickAddBtn();
+    choosePlaylist();
+        Assert.assertTrue(getNotificationText().contains(newSongAddedNotificationText));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
